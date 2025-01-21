481792ceda0be9ad053188a13e966645ed24ea04
David Fernández <davidfernandezcomesana@gmail.com>
2025-01-21 20:55:06 +0100

    test

---
+++ test.js
@@ -0,0 +1,6 @@
+const toCamelCase = require("./index");
+
+// Pruebas
+console.log(toCamelCase("hello world"));
+console.log(toCamelCase("transform_this_text"));
+console.log(toCamelCase("to-camel-case"));
